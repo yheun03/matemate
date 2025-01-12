531b16a6e8050d4510950edb980f8b3a97da8206
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2025-01-12 21:15:53 +0900

    체크리스트 및 js 수정

--- public/src/assets/scripts/common.js
+++ public/src/assets/scripts/common.js
@@ -11,38 +11,4 @@ document.addEventListener('DOMContentLoaded', () => {
             type: 'progressbar', // 진행도 표시
         },
     });
-
-    // 유저 이미지 변경
-    window.addEventListener('DOMContentLoaded', function() {
-        document.getElementById('fileInput').addEventListener('change', function() {
-            const file = this.files[0];
-            if (file) {
-                const preview = document.getElementById('preview');
-                const objectURL = URL.createObjectURL(file);  // 임시 URL 생성
-                preview.src = objectURL;
-
-                // 메모리 누수를 방지하기 위해 이미지 로드 후 URL 해제
-                preview.onload = function() {
-                URL.revokeObjectURL(objectURL);
-                };
-            } else {
-                document.getElementById('preview').src = "";
-            }
-        });
-    });
-    const rangeInputs = document.querySelectorAll("input[type='range']");
-
-    rangeInputs.forEach(function (rangeInput) {
-        rangeInput.addEventListener("input", function () {
-            const value = this.value;
-
-            if (value === "0") {
-                this.className = "bad";
-            } else if (value === "100") {
-                this.className = "good";
-            } else {
-                this.className= "";
-            }
-        });
-    });
 });
