0028e69fbec3605a35cf638af109e5a8a48357bf
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2025-01-19 23:24:45 +0900

    파일명 수정 및 채팅 리스트 추가

--- public/src/assets/scripts/common.js
+++ public/src/assets/scripts/common.js
@@ -1,14 +1,2 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper', {
-        centeredSlides: true,
-        spaceBetween: 30,
-
-        navigation: {
-          nextEl: '.gotoNext',
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'progressbar', // 진행도 표시
-        },
-    });
 });
